3d8b9fa3bfdf37a755fddfe9b96a12ef590253f9
FelipePrometti <felipe.prometti@gmail.com>
2024-03-14 10:08:53 -0300

    calculo do dolar 2

--- src/TesteMoeda.java
+++ src/TesteMoeda.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class TesteMoeda {
@@ -8,12 +9,23 @@ public class TesteMoeda {
         moeda.valorReal = leitor.nextDouble();
         System.out.println("Informe a cotação do dólar de hoje: ");
         moeda.cotacaoDolar = leitor.nextDouble();
-        moeda.calcularConversao();
-        System.out.println(moeda.quantidadeDolar);
-        System.out.println(moeda.retornarCalculoConversao());
+        //moeda.calcularConversao();
+        //System.out.println(moeda.quantidadeDolar);
+        System.out.println(moeda.comprarDolar("20755197211", "Felipe Prometti"));
+        System.out.println("----------------------------------------------------------------------");
 
-
-
+        Moeda moeda2 = new Moeda();
+        //System.out.println(moeda.retornarCalculoConversao());
+        String nome, cpf;
+        System.out.println("Informe o nome do cliente: ");
+        nome = leitorTexto.nextLine();
+        System.out.println("Informe o cpf do cliente: ");
+        cpf = leitorTexto.next();
+        System.out.println("Informe o valor em reais: R$ ");
+        moeda2.valorReal = leitor.nextDouble();
+        System.out.println("Informe a cotação do dólar hoje: ");
+        moeda2.cotacaoDolar = leitor.nextDouble();
+        System.out.println(moeda2.comprarDolar((nome, cpf));
 
     }
 }
